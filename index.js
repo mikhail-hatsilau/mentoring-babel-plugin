1a3fe9df62854a19bcec5a2ba7714072ecca405f
Mikhail Hatsilau <Mikhail_Hatsilau@epam.com>
2016-11-25 17:14:26 +0300

    Added handling of additional cases

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-const isPropertyNodeEnv = (path) =>
-    path.get('property').isIdentifier({ name: 'NODE_ENV' });
+const isCorrectProperty = (property) =>
+    property.isIdentifier() || property.isStringLiteral();
+
+const getPropertyName = (propertyNode) =>
+    propertyNode.name || propertyNode.value;
 
 const isObjectProcessEnv = (path) =>
     path.get('object').isIdentifier({ name: 'process' }) &&
     path.get('property').isIdentifier({ name: 'env' });
 
-const getEnvValue = () =>
-    process.env.NODE_ENV || 'development';
+const getNode = (types, value) =>
+    value === undefined ? types.identifier('undefined') : types.stringLiteral(value);
 
 module.exports = ({ types: t }) => (
      {
         visitor: {
             MemberExpression(path) {
                 const expressionObject = path.get('object');
-                if (isPropertyNodeEnv(path) && isObjectProcessEnv(expressionObject)) {
-                    path.replaceWith(t.stringLiteral(getEnvValue()));
+                const property = path.get('property');
+                if(!t.isAssignmentExpression(path.parent)) {
+                    if (isObjectProcessEnv(expressionObject) && isCorrectProperty(property) ) {
+                        const propertyName = getPropertyName(property.node);
+                        const value = process.env[propertyName];
+                        path.replaceWith(getNode(t, value));
+                    }
                 }
             }
         }
--- src/example.js
+++ src/example.js
@@ -1,3 +1,8 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production') {
     console.log('Congratulations! You are using production env');
 }
+obj.process.env.NODE_ENV;
+process.env.NODE_ENV;
+process.env['NODE_ENV'];
+process.env.NODE_ENV = 'abs';
+process.env.CUSTOM;
